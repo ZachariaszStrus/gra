e5386ae7250d8b87b5ce5b46ccb7e2b3340977cc
Dawid Zwoliński <zwola118@hotmail.com>
2016-04-09 17:50:29 +0200

    sender bitches

--- Client/Sender.py
+++ Client/Sender.py
@@ -1,12 +1,48 @@
+import threading
 import socket
+
+
+class Sender:
+
+    def __init__(self, adres):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect(adres)
+
+        #("localhost", 9876)
+
+    def send(self, index):
+        self.server.send(index)
+
+    def close(self):
+        self.server.close()
+
+
+class Listener (threading.Thread):
+
+    def __init__(self, adres):
+        threading.Thread.__init__(self)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.connect(adres)
+
+        #("localhost", 9876)
+
+    def run(self):
+        while 1:
+            print(self.server.listen())
+        self.server.close()
+
+
+
+
 
 
 class Sender:
     SERVER = ("localhost", 9876)
 
-    @staticmethod
-    def send(key):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(Sender.SERVER)
-        sock.send(bytes(key))
-        sock.close()
+    def __init__(self):
+
+
+        # sock.send(bytes(key)
+
+    def close(self):
+
