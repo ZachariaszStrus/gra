3e8677afdda8bd1237ceb81846fc4d6ba466911f
ZachariaszStrus <zachariasz.strus@gmail.com>
2016-04-11 17:30:03 +0200

    nic

--- Client/Listener.py
+++ Client/Listener.py
@@ -17,10 +17,7 @@ class Listener (threading.Thread):
     def run(self):
         buf = bytearray(4096)
         while self.running:
-            size = self.socket.recv_into(buf)
-            if size > 0:
-                for i in range(size):
-                    buf[i] = int(buf[i]) - 48
-                print "Received :", struct.unpack("<I", buf[0:4])[0]
+            if self.socket.recv_into(buf) > 0:
+                print "Received :", buf
         self.socket.close()
 
