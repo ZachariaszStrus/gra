e251fcccc40cf3c25c5e42d7c776d68b257e9faa
Dawid Zwoliński <zwola118@hotmail.com>
2016-04-11 23:03:10 +0200

    New network protocol

--- Client/Listener.py
+++ Client/Listener.py
@@ -1,25 +1,50 @@
-import socket
+from socket import *
 import threading
-
 import struct
 
 
 class Listener (threading.Thread):
-    def __init__(self, address=("25.37.158.69", 9999)):
+    def __init__(self, address=("localhost", 9999)):
         threading.Thread.__init__(self)
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.socket = socket(AF_INET, SOCK_STREAM)
         self.socket.connect(address)
         self.running = True
 
     def stop(self):
         self.running = False
 
+    def get_string(self, i):
+        size = i
+        tab = list()
+        while size > 0:
+            if size < 100:
+                buf = bytearray(size)
+                size = 0
+            else:
+                buf = bytearray(100)
+                size -= 100
+
+            while 1:
+                if self.socket.recv_into(buf) > 0:
+                    tab.append(buf)
+                    break
+
+        result = ''
+        for s in tab:
+            result += s.decode('utf-8')
+
+        return result
+
     def run(self):
-        buf = bytearray(4096)
+        buf = bytearray(4)
         while self.running:
-            size = self.socket.recv_into(buf)
-            if size > 0:
-                print "Received :", buf
-                print "Size :", size
+            if self.socket.recv_into(buf) > 0:
+                num = struct.unpack("!i", buf)[0]
+                print(num)
+                print(self.get_string(num))
+
         self.socket.close()
+
+
 
