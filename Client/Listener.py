a3e899e128796e5410ab0bad1e1bea2593c4c901
ZachariaszStrus <zachariasz.strus@gmail.com>
2016-04-11 17:56:43 +0200

    nic

--- Client/Listener.py
+++ Client/Listener.py
@@ -5,7 +5,7 @@ import struct
 
 
 class Listener (threading.Thread):
-    def __init__(self, address=("localhost", 9999)):
+    def __init__(self, address=("25.37.158.69", 9999)):
         threading.Thread.__init__(self)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(address)
@@ -17,7 +17,9 @@ class Listener (threading.Thread):
     def run(self):
         buf = bytearray(4096)
         while self.running:
-            if self.socket.recv_into(buf) > 0:
+            size = self.socket.recv_into(buf)
+            if size > 0:
                 print "Received :", buf
+                print "Size :", size
         self.socket.close()
 
--- Client/Sender.py
+++ Client/Sender.py
@@ -4,12 +4,13 @@ import socket
 
 class Sender:
 
-    def __init__(self, address=("localhost", 9998)):
+    def __init__(self, address=("25.37.158.69", 9998)):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(address)
 
     def send(self, key):
         self.socket.send(bytes(self.get_key_index(key)))
+        self.socket.send(bytes(0))
         print "Sent : ", self.get_key_index(key)
 
     def close(self):
