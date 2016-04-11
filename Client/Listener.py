bd858a7173220a814ea20152072d440a510e30f2
Dawid <zwola118@hotmail.com>
2016-04-11 13:34:10 +0200

    Update Listener.py

--- Client/Listener.py
+++ Client/Listener.py
@@ -3,7 +3,7 @@ import threading
 
 
 class Listener (threading.Thread):
-    def __init__(self, address=("localhost", 9999)):
+    def __init__(self, address=("42.0.174.162", 9999)):
         threading.Thread.__init__(self)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(address)
